b7a6b12dcfc45ef9ce7dc1147ad2c9449df241b2
Dante-Coshiwe <dantecoshiwe@icloud.com>
2026-02-14 14:18:31 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+
+const app = express();
+app.use(express.json());
+
+app.post("/", (req, res) => {
+  const { data } = req.body;
+
+  if (!data || typeof data !== "string") {
+    return res.status(400).json({ error: "You did the wrong thing Trash panda, input a string and be a proper raccoon whisperer" });
+  }
+
+  const sortedArray = data.toLowerCase().trim().split("").sort();
+
+  return res.json({ word: sortedArray });
+/*
+});
+  app.listen(3000, () => {
+  console.log("Server running on port 3000");
+*/
+});
+
+module.exports = app;
